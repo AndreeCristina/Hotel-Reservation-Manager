75be0cee8e4a1af90a7c9b28fe8564164d0f18ac
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-10-04 14:58:43 +0300

    Upload project

---
+++ src/main/java/com/itschool/hotelResvMgt/HotelResvMgrApplication.java
@@ -0,0 +1,13 @@
+package com.itschool.hotelResvMgt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelResvMgrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelResvMgrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/itschool/hotelResvMgt/HotelResvMgtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itschool.hotelResvMgt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelResvMgtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
