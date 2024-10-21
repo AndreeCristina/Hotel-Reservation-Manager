ae74e1f144de85695627282c1fcf8d57eaed13d5
AndreeCristina <cepoiandreeacristina@gmail.com>
2024-10-21 14:46:15 +0300

    changes

--- src/main/java/com/itschool/hotelResvMgt/controllers/RoomController.java
+++ src/main/java/com/itschool/hotelResvMgt/controllers/RoomController.java
@@ -1,11 +1,11 @@
 package com.itschool.hotelResvMgt.controllers;
 
 import com.itschool.hotelResvMgt.models.dtos.RoomDTO;
-import com.itschool.hotelResvMgt.models.entities.Room;
 import com.itschool.hotelResvMgt.services.RoomService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
